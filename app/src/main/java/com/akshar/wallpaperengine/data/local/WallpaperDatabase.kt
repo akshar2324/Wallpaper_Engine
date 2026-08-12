58afda5d56227205e0b9bb32f413c2b28687cceb
Akshar Kalathiya <90042795+akshar2324@users.noreply.github.com>
2026-08-12 07:20:36 +0530

    feat(database): increment schema version
    Bump Room database version to 2 to support upcoming schema migrations.

--- app/src/main/java/com/akshar/wallpaperengine/data/local/WallpaperDatabase.kt
+++ app/src/main/java/com/akshar/wallpaperengine/data/local/WallpaperDatabase.kt
@@ -17,7 +17,7 @@ import com.akshar.wallpaperengine.data.local.entity.*
         ScheduleEntity::class,
         WallpaperHistoryEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class WallpaperDatabase : RoomDatabase() {
